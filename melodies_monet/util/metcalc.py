bc37db31ffb77b7167a96c530d392edfe9634891
Becky Schwantes <38259446+rschwant@users.noreply.github.com>
2026-04-28 09:16:45 -0600

    Update melodies_monet/util/metcalc.py
    Co-authored-by: Pablo Lichtig <pablo.lichtig@gmail.com>

--- melodies_monet/util/metcalc.py
+++ melodies_monet/util/metcalc.py
@@ -49,6 +49,7 @@ def dewpoint(obj, varmap = None, output_key = "dewpoint"):
         raise ImportError(
             "metpy is required for extra_calc. "
             "Install with: conda install -c conda-forge metpy"
+            "or with: pip install metpy"
         )
     
     # grab variable names from the yaml and error if not provided
