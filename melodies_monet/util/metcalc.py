08b0c4063497c6e6720834744f390fe722411d8e
Becky Schwantes <38259446+rschwant@users.noreply.github.com>
2026-04-28 09:17:19 -0600

    Update melodies_monet/util/metcalc.py
    Co-authored-by: Pablo Lichtig <pablo.lichtig@gmail.com>

--- melodies_monet/util/metcalc.py
+++ melodies_monet/util/metcalc.py
@@ -97,6 +97,7 @@ def relh(obj, varmap=None, output_key="rel_hum"):
         raise ImportError(
             "metpy is required for extra_calc. "
             "Install with: conda install -c conda-forge metpy"
+            "or with: pip install metpy"
         )
 
     # grab variable names from the yaml and error if not provided
