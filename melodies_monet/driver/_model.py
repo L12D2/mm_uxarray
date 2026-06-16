2f307e9f90c515e80bb7b1ed15a94e56737f4bc5
Liam Thompson <lcthompson@crhtc66.hpc.ucar.edu>
2026-06-15 19:47:15 -0600

    updating yaml with correct indexing

--- melodies_monet/driver/_model.py
+++ melodies_monet/driver/_model.py
@@ -238,12 +238,7 @@ class model:
                 self.obj = mio.fv3chem.open_dataset(self.files, **self.mod_kwargs)
         elif "cesm_fv" in self.model.lower():
             print("**** Reading CESM FV model output...")
-            self.mod_kwargs.update(
-                {
-                    "var_list": list_input_var,
-                    "surf_only": control_dict["model"][self.label].get("surf_only", False),
-                }
-            )
+            self.mod_kwargs.update({"var_list": list_input_var})
             try:
                 self.obj = mio.models.cesm_fv.open_mfdataset(self.files, **self.mod_kwargs)
             except AttributeError:
